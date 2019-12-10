59a1c70e1f3e5c77dfd65abe8d39f54ff505e660
IamDucloux <ianducloux@gmail.com>
2019-12-09 23:23:04 -0600

    Agregar archivos de proyecto.

---
+++ Supreme TPS/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supreme_TPS
+{
+    class Usuario
+    {
+        private string nombre;
+        private string contraseña;
+        private char nivel;
+
+    }
+}
